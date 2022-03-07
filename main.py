445efabd1dad39289602cbb490b1d5a633822f5d
lrodrz <one@POSEIDON-pi.attlocal.net>
2022-03-07 08:32:05 +0000

    clean

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 #! usr/bin/env python
+
+#import initenv  # will work on this later as an enhancement 
+#. importing works like this : from application.app.folder.file import func_name
+import tools.DataCleaning.getfeeds as gf 
+import tools.DataCleaning.webpage as wp
+ 
+#get me my rss feeds
+#gf.linker()
+
+#get me my news page links
+#gf.ParseXML()
+
+#parse the webpage
+wp.Webpage()
+
+
+
+
